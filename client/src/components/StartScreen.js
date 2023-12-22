d25e139cff8504fb436591f9d218338373f3bd0b
Vikramaditya <awesomevikram3@gmail.com>
2023-12-22 20:53:57 +0530

    feat:corrected the user page
    Signed-off-by: Vikramaditya <awesomevikram3@gmail.com>

--- client/src/components/StartScreen.js
+++ client/src/components/StartScreen.js
@@ -18,7 +18,7 @@ function StartScreen({ onStart }) {
         );
         
         setUser(response.data);
-        console.log(response.data);
+      
   
         // Check if 'points' is not null or undefined before accessing it
         if (response.data && response.data.points !== undefined && response.data.points !== null && response.data.points !== -1) {
